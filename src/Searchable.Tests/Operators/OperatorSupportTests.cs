d4a3555459ec3546ac8f800e5ee8ba70f3fa9580
John Hurrell <jhurrell@gmail.com>
2013-02-23 23:49:48 -0500

    Started adding support and tests for the mappings between types and the operators they support.

---
+++ src/Searchable.Tests/Operators/OperatorSupportTests.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Searchable.Operators;
+
+namespace SearchableTests.Operators
+{
+	[TestClass]
+	public class OperatorSupportTests
+	{
+		protected OperatorSupport target { get; set; }
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			target = new OperatorSupport();
+		}
+
+		[TestClass]
+		public class IndexerTests : OperatorSupportTests
+		{
+			[TestMethod]
+			[ExpectedException(typeof(ArgumentException))]
+			public void Throw_Exception_Given_Unsupported_Type()
+			{
+				var result = target[typeof(OperatorSupport)];
+			}
+		}
+
+		[TestClass]
+		public class TypesSupportedTests : OperatorSupportTests
+		{
+			[TestMethod]
+			public void Bool()
+			{
+				Assert.IsNotNull(target[typeof(bool)]);
+			}
+
+			[TestMethod]
+			public void Byte()
+			{
+				Assert.IsNotNull(target[typeof(byte)]);
+			}
+
+			[TestMethod]
+			public void Char()
+			{
+				Assert.IsNotNull(target[typeof(char)]);
+			}
+
+			[TestMethod]
+			public void DateTime()
+			{
+				Assert.IsNotNull(target[typeof(DateTime)]);
+			}
+
+			[TestMethod]
+			public void Decimal()
+			{
+				Assert.IsNotNull(target[typeof(decimal)]);
+			}
+
+			[TestMethod]
+			public void Double()
+			{
+				Assert.IsNotNull(target[typeof(double)]);
+			}
+
+			[TestMethod]
+			public void Float()
+			{
+				Assert.IsNotNull(target[typeof(float)]);
+			}
+
+			[TestMethod]
+			public void Int()
+			{
+				Assert.IsNotNull(target[typeof(int)]);
+			}
+
+			[TestMethod]
+			public void Long()
+			{
+				Assert.IsNotNull(target[typeof(long)]);
+			}
+
+			[TestMethod]
+			public void Sbyte()
+			{
+				Assert.IsNotNull(target[typeof(sbyte)]);
+			}
+
+			[TestMethod]
+			public void Short()
+			{
+				Assert.IsNotNull(target[typeof(short)]);
+			}
+
+			[TestMethod]
+			public void String()
+			{
+				Assert.IsNotNull(target[typeof(string)]);
+			}
+
+			[TestMethod]
+			public void Uint()
+			{
+				Assert.IsNotNull(target[typeof(uint)]);
+			}
+
+			[TestMethod]
+			public void Ulong()
+			{
+				Assert.IsNotNull(target[typeof(ulong)]);
+			}
+
+			[TestMethod]
+			public void Ushort()
+			{
+				Assert.IsNotNull(target[typeof(ushort)]);
+			}
+
+			[TestMethod]
+			public void NullableBool()
+			{
+				Assert.IsNotNull(target[typeof(bool?)]);
+			}
+
+			[TestMethod]
+			public void NullableByte()
+			{
+				Assert.IsNotNull(target[typeof(byte?)]);
+			}
+
+			[TestMethod]
+			public void NullableChar()
+			{
+				Assert.IsNotNull(target[typeof(char?)]);
+			}
+
+			[TestMethod]
+			public void NullableDateTime()
+			{
+				Assert.IsNotNull(target[typeof(DateTime?)]);
+			}
+
+			[TestMethod]
+			public void NullableDecimal()
+			{
+				Assert.IsNotNull(target[typeof(decimal?)]);
+			}
+
+			[TestMethod]
+			public void NullableDouble()
+			{
+				Assert.IsNotNull(target[typeof(double?)]);
+			}
+
+			[TestMethod]
+			public void NullableFloat()
+			{
+				Assert.IsNotNull(target[typeof(float?)]);
+			}
+
+			[TestMethod]
+			public void NullableInt()
+			{
+				Assert.IsNotNull(target[typeof(int?)]);
+			}
+
+			[TestMethod]
+			public void NullableLong()
+			{
+				Assert.IsNotNull(target[typeof(long?)]);
+			}
+
+			[TestMethod]
+			public void NullableShotr()
+			{
+				Assert.IsNotNull(target[typeof(short?)]);
+			}
+
+			[TestMethod]
+			public void NullableSbyte()
+			{
+				Assert.IsNotNull(target[typeof(sbyte?)]);
+			}
+
+			[TestMethod]
+			public void NullableUint()
+			{
+				Assert.IsNotNull(target[typeof(uint?)]);
+			}
+
+			[TestMethod]
+			public void NullableUlong()
+			{
+				Assert.IsNotNull(target[typeof(ulong?)]);
+			}
+
+			[TestMethod]
+			public void NullableUshort()
+			{
+				Assert.IsNotNull(target[typeof(ushort?)]);
+			}
+		}
+
+		[TestClass]
+		public class StringTypeTests : OperatorSupportTests
+		{
+
+			
+		}
+	}
+}
---
+++ src/Searchable/Operators/OperatorSupport.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Searchable.Operators
+{
+	public class OperatorSupport
+	{
+		public Dictionary<Type, object> Types { get; private set; }
+
+		public OperatorSupport()
+		{
+			Types = new Dictionary<Type, object>();
+
+			// Numeric types.
+			Types.Add(typeof(bool), new object());
+			Types.Add(typeof(byte), new object());
+			Types.Add(typeof(char), new object());
+			Types.Add(typeof(DateTime), new object());
+			Types.Add(typeof(decimal), new object());
+			Types.Add(typeof(double), new object());
+			Types.Add(typeof(float), new object());
+			Types.Add(typeof(int), new object());
+			Types.Add(typeof(long), new object());
+			Types.Add(typeof(sbyte), new object());
+			Types.Add(typeof(short), new object());
+			Types.Add(typeof(uint), new object());
+			Types.Add(typeof(ulong), new object());
+			Types.Add(typeof(ushort), new object());
+			Types.Add(typeof(bool?), new object());
+			Types.Add(typeof(byte?), new object());
+			Types.Add(typeof(char?), new object());
+			Types.Add(typeof(DateTime?), new object());
+			Types.Add(typeof(decimal?), new object());
+			Types.Add(typeof(double?), new object());
+			Types.Add(typeof(float?), new object());
+			Types.Add(typeof(int?), new object());
+			Types.Add(typeof(long?), new object());
+			Types.Add(typeof(sbyte?), new object());
+			Types.Add(typeof(short?), new object());
+			Types.Add(typeof(uint?), new object());
+			Types.Add(typeof(ulong?), new object());
+			Types.Add(typeof(ushort?), new object());
+
+			Types.Add(typeof(string), new object());
+
+
+		}
+
+		public object this[Type type]
+		{
+			get
+			{
+				if(!Types.ContainsKey(type))
+					throw new ArgumentException(string.Format("The type specified, ({0}) is not supported.", type));
+
+				return Types[type];
+			}
+		}
+	}
+}
